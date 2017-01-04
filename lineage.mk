135389e212aeae3b00bddbb2bda7f56914349684
Kilian von Pflugk <kilian@nood.tv>
2017-01-04 09:12:09 +0100

    r7: Rebrand to LineageOS
    Change-Id: Ia995011eb85aa94a6598c48e9b7e193d9e7b3371

--- lineage.mk
+++ lineage.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from r7 device
 $(call inherit-product, device/oppo/r7/device.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_r7
+PRODUCT_NAME := lineage_r7
 PRODUCT_DEVICE := r7
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := R7f
